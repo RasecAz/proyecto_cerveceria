d1e307f8a0381d67f9c27a60a96986c98112a431
Cesar Augusto Ariza Duarte <cesar.ariza11@outlook.com>
2026-06-23 18:54:36 -0500

    Cambio del logo perosnalizado en el Navbar

--- src/presentation/components/Navbar/Navbar.tsx
+++ src/presentation/components/Navbar/Navbar.tsx
@@ -1,17 +1,24 @@
-import styles from './Navbar.module.css';
+import logo from "../../../assets/Logo.png";
+import styles from "./Navbar.module.css";
 
 export function Navbar() {
   return (
     <nav className={styles.navbar}>
-      <div className={styles.logo}>
-        <div className={styles.logoIcon}>W</div>
-        <span className={styles.logoText}>Wajira</span>
+      <div className={styles.logoContainer}>
+        <img
+          src={logo}
+          alt="Wajira Logo"
+          className={styles.logo}
+        />
+
+        <h1 className={styles.brand}>Wajira</h1>
       </div>
-      <div className={styles.links}>
-        <a href="#cervezas">Cervezas</a>
-        <a href="#nosotros">Nosotros</a>
-        <a href="#contacto">Contacto</a>
-      </div>
+
+      <ul className={styles.navLinks}>
+        <li>Cervezas</li>
+        <li>Nosotros</li>
+        <li>Contacto</li>
+      </ul>
     </nav>
   );
 }
